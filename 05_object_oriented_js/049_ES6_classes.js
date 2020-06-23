3b9b53e30e155ba0d072586c52b0b9273526a649
akhil-kk <akhilkkopparambil@gmail.com>
2020-06-23 22:22:54 +0530

    Object oriented JS final

---
+++ 05_object_oriented_js/049_ES6_classes.js
@@ -0,0 +1,32 @@
+class Person {
+    constructor(firstName, lastName, dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    greeting() {
+        return `Hello there ${this.firstName} ${this.lastName}`
+    }
+
+    claculateAge() {
+        const diff = Date.now() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+
+    getsMarried(newLastname) {
+      this.lastName = newLastname;  
+    }
+
+    static addNumbers(x, y) {
+        return x + y;
+    }
+}
+
+const mary = new Person('Mary', 'Williams', '11-13-1980');
+
+mary.getsMarried('Thompson');
+
+console.log(mary);
+
+console.log(Person.addNumbers(1, 2));
