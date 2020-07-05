738fff79260a12642b0783ed43139c46384ad74c
akhil-kk <akhilkkopparambil@gmail.com>
2020-07-05 23:46:51 +0530

    Video progress bar and time stamp

--- 13_custom_video_player_with_HTML5_API/app.js
+++ 13_custom_video_player_with_HTML5_API/app.js
@@ -5,9 +5,8 @@ const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
 // Play and pause video
-
 function toggleVideoStatus() {
-    if(video.paused) {
+    if (video.paused) {
         video.play();
     } else {
         video.pause();
@@ -15,35 +14,43 @@ function toggleVideoStatus() {
 }
 
 // Update play/progress icon
-
 function updatePlayIcon() {
     if (video.paused) {
         play.innerHTML = '<i class = "fa fa-play fa-2x"></i>';
     } else {
-        play.innerHTML = '<i class = "fa fa-pause fa-2x"></i>'; 
+        play.innerHTML = '<i class = "fa fa-pause fa-2x"></i>';
     }
 }
 
 // Update progress & timestamp
-
 function updateProgress() {
+    progress.value = (video.currentTime / video.duration) * 100;
 
+    //get minutes
+    let mins = Math.floor(video.currentTime / 60);
+    if (mins < 10) {
+        mins = '0' + String(mins);
+    }
+
+    //get seconds
+    let secs = Math.floor(video.currentTime % 60);
+    if (secs < 10) {
+        secs = '0' + String(secs);
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // set video time to progress
-
 function setVideoProgress() {
-    return true;
+    video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // stop video 
-
 function stopVideo() {
     video.currentTime = 0;
     video.pause();
 }
-
-
 
 // Event listeners
 
