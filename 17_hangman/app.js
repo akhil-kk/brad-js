654c9d33c4783dc9548ee0e7ef93e7f16e36898d
akhil-kk <akhilkkopparambil@gmail.com>
2020-07-08 10:41:52 +0530

    Wrong Letters Play Again

--- 17_hangman/app.js
+++ 17_hangman/app.js
@@ -35,11 +35,41 @@ function displayWord() {
   }
 }
 
+
+// update wrong letters
+
+function updateWrongLettersEl() {
+  // display wrong letters
+
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `;
+
+  // display parts
+
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if (index < errors) {
+      part.style.display = 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  });
+
+  // check if lost
+
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'Unfortunately you lost.';
+    popup.style.display = 'flex';
+  }
+}
+
 // show notification
 
 function showNotification() {
   notification.classList.add('show');
-
   setTimeout(() => {
     notification.classList.remove('show');
   }, 2000);
@@ -69,6 +99,18 @@ window.addEventListener('keydown', e => {
       }
     }
   }
+});
+
+// restart game and play again
+playAgainBtn.addEventListener('click', () => {
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+  updateWrongLettersEl();
+
+  popup.style.display = 'none';
 });
 
 displayWord();
