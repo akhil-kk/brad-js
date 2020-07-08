36d1c159face8815d78121b5991e67e01367b708
akhil-kk <akhilkkopparambil@gmail.com>
2020-07-08 10:25:40 +0530

    Letter Press Event Handler

--- 17_hangman/app.js
+++ 17_hangman/app.js
@@ -12,28 +12,63 @@ const words = ['wizard', 'witch', 'Arjun', 'Akhil', "Don"];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-const correctLetters = ['A','k','h','i','l'];
+const correctLetters = [];//'A','k','h','i','l'
 const wrongLetters = [];
 
 // show the hidden word
 function displayWord() {
-    wordEl.innerHTML = `
+  wordEl.innerHTML = `
       ${selectedWord.split('').map(letter => `
             <span class="letter">
               ${correctLetters.includes(letter) ? letter : ''}
             </span>
           `
-        )
-        .join('')}
+  )
+      .join('')}
     `;
 
-    const innerWord = wordEl.innerText.replace(/\n/g, '');
+  const innerWord = wordEl.innerText.replace(/\n/g, '');
 
-    if (innerWord === selectedWord) {
-      finalMessage.innerText = 'Congratulations. You won';
-      popup.style.display = 'flex';
-    }
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = 'Congratulations. You won';
+    popup.style.display = 'flex';
+  }
 }
 
+// show notification
+
+function showNotification() {
+  notification.classList.add('show');
+
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 2000);
+}
+
+// key down letter press
+
+window.addEventListener('keydown', e => {
+  // console.log(e.keyCode);
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
+
 displayWord();
-  
