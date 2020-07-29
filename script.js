9946d4ccc5768290f0192891d2b5fd9024bbc448
akhil-kk <akhilkkopparambil@gmail.com>
2020-07-29 23:54:58 +0530

    variable declarations

---
+++ script.js
@@ -0,0 +1,23 @@
+// setTimeout(wakeUpUser, 5000);
+
+// function wakeUpUser() {
+//     alert('Are you going to stare at this boring page forever')
+// }
+
+let price = 28.99 // cretae a variable and assign value 28.99 to it.
+let discount = 10; // cretae a variable and assign value 10 to it.
+
+let total = (price * (discount/100)); // calculate price by applying disocount.
+
+if(total>25) {
+    freeshipping();  // compare total with 25. if greater then call freeshipping
+}
+
+
+let count = 10; // create variable count and assign 10 to it.
+while (count>0) {
+    juggle();
+    count = count -1;
+} // as long as count is greater than 0 call juggle and after reduce count by 1.
+
+let dog = {name: "Roger", weight: 35}; // create an objcet called dog and give name and weight.
