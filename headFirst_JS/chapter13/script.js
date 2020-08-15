c717cf5773c9dcc646a025b86f788a5ede67522b
akhil-kk <akhilkkopparambil@gmail.com>
2020-08-15 17:35:42 +0530

    chapter13_prototypes

--- headFirst_JS/chapter13/script.js
+++ headFirst_JS/chapter13/script.js
@@ -17,6 +17,72 @@ let fluffy = new Dog("Fluffy", "Poodle", 30);
 let spot = new Dog("Spot", "Chihuahua", 10);
 console.log(fido.bark());
 
+
+function ShowDog(name, breed, weight, handler) {
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+    this.handler = handler;
+}
+
+ShowDog.prototype = new Dog();
+Showdog.prototype.league = "Webville";
+ShowDog.prototype.stack = function () {
+    console.log("Stack");
+};
+
+ShowDog.prototype.bait = function () {
+    console.log("Bait");
+};
+
+ShowDog.prototype.gait = function (kind) {
+    console.log(kind + "ing");
+};
+
+ShowDog.prototype.groom = function () {
+    console.log("Groom");
+};
+
+// excercise 
+
+function Robot(name, year, owner) {
+    this.name = name;
+    this.year = year;
+    this.owner = owner;
+}
+
+Robot.prototype.maker = "ObjectsRUs";
+Robot.prototype.speak = function () {
+    alert("Warning warning!!");
+};
+
+Robot.prototype.makeCoffee = function () {
+    alert("Making coffee");
+};
+
+Robot.prototype.blinkLights = function () {
+    alert("Blink blink!");
+};
+
+let robby = new Robot("Robby", 1956, "Dr. Morbius");
+let rosie = new Robot("Rosie", 1962, "George Jetson");
+robby.onOffSwitch = true;
+robby.makeCoffee = function () {
+    alert("Fetching a coffee from Starbucks.");
+};
+
+rosie.cleanHouse = function () {
+    alert("Cleaning! Spic and Span soon...");
+};
+
+console.log(robby.name + " was made by " + robby.maker +
+    " in " + robby.year + " and is owned by " + robby.owner);
+robby.makeCoffee();
+robby.blinkLights();
+console.log(rosie.name + " was made by " + rosie.maker +
+    " in " + rosie.year + " and is owned by " + rosie.owner);
+rosie.cleanHouse();
+
 // using prototypes
 
 function Dog(name, breed, weight) {
@@ -138,3 +204,118 @@ rosie.reportError();
 robby.reportError();
 console.log(robby.hasOwnProperty("errorMessage"));
 console.log(rosie.hasOwnProperty("errorMessage"));
+
+let fido = new Dog("Fido", "Mixed", 38);
+
+if (fido instanceof Dog) {
+    console.log("Fido is a Dog");
+}
+
+if (fido instanceof ShowDog) {
+    console.log("Fido is a ShowDog");
+}
+
+let scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
+if (scotty instanceof Dog) {
+    console.log("Scotty is a Dog");
+}
+
+if (scotty instanceof ShowDog) {
+    console.log("Scotty is a ShowDog");
+}
+
+console.log("Fido constructor is " + fido.constructor);
+console.log("Scotty constructor is " + scotty.constructor);
+
+//inheritence
+
+function Dog(name, breed, weight) {
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+}
+
+Dog.prototype.species = "Canine";
+
+Dog.prototype.bark = function () {
+    if (this.weight > 25) {
+        console.log(this.name + " says Woof!");
+    } else {
+        console.log(this.name + " says Yip!");
+    }
+};
+
+Dog.prototype.run = function () {
+    console.log("Run!");
+};
+
+Dog.prototype.wag = function () {
+    console.log("Wag!");
+};
+
+function Dog(name, breed, weight) {
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+}
+
+Dog.prototype.species = "Canine";
+
+Dog.prototype.bark = function () {
+    if (this.weight > 25) {
+        console.log(this.name + " says Woof!");
+    } else {
+        console.log(this.name + " says Yip!");
+    }
+};
+
+Dog.prototype.run = function () {
+    console.log("Run!");
+};
+
+Dog.prototype.wag = function () {
+    console.log("Wag!");
+};
+
+function ShowDog(name, breed, weight, handler) {
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+    this.handler = handler;
+}
+
+ShowDog.prototype = new Dog();
+
+ShowDog.prototype.league = "Webville";
+
+ShowDog.prototype.stack = function () {
+    console.log("Stack");
+};
+
+ShowDog.prototype.bait = function () {
+    console.log("Bait");
+};
+
+ShowDog.prototype.gait = function (kind) {
+    console.log(kind + "ing");
+};
+
+ShowDog.prototype.groom = function () {
+    console.log("Groom");
+};
+
+let scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
+scotty.stack();
+scotty.bark();
+console.log(scotty.league);
+console.log(scotty.species);
+
+// instance of
+
+let fido = new Dog("Fido", "Mixed", 38);
+if (fido instanceof Dog) {
+console.log("Fido is a Dog");
+}
+if (fido instanceof ShowDog) {
+console.log("Fido is a ShowDog");
+}
