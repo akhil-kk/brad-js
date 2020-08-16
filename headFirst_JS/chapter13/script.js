963200e289fd0c9c323cfb6624c399b5c37f3a0c
akhil-kk <akhilkkopparambil@gmail.com>
2020-08-16 22:22:30 +0530

    chapter13 final

--- headFirst_JS/chapter13/script.js
+++ headFirst_JS/chapter13/script.js
@@ -314,8 +314,47 @@ console.log(scotty.species);
 
 let fido = new Dog("Fido", "Mixed", 38);
 if (fido instanceof Dog) {
-console.log("Fido is a Dog");
+    console.log("Fido is a Dog");
 }
 if (fido instanceof ShowDog) {
-console.log("Fido is a ShowDog");
+    console.log("Fido is a ShowDog");
 }
+
+//test drive
+
+function ShowDog(name, breed, weight, handler) {
+    Dog.call(this, name, breed, weight);
+    this.handler = handler;
+}
+
+ShowDog.prototype = new Dog();
+ShowDog.prototype.constructor = ShowDog;
+ShowDog.prototype.league = "Webville";
+ShowDog.prototype.stack = function () {
+    console.log("Stack");
+};
+
+ShowDog.prototype.bait = function () {
+    console.log("Bait");
+};
+
+ShowDog.prototype.gait = function (kind) {
+    console.log(kind + "ing");
+};
+
+ShowDog.prototype.groom = function () {
+    console.log("Groom");
+};
+
+let fido = new Dog("Fido", "Mixed", 38);
+let fluffy = new Dog("Fluffy", "Poodle", 30);
+let spot = new Dog("Spot", "Chihuahua", 10);
+let scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
+let beatrice = new ShowDog("Beatrice", "Pomeranian", 5, "Hamilton");
+fido.bark();
+fluffy.bark();
+spot.bark();
+scotty.bark();
+beatrice.bark();
+scotty.gait("Walk");
+beatrice.groom();
