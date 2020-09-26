8ae302a97a67519ee7078424b727e70cf5c49f9b
akhil-kk <akhilkkopparambil@gmail.com>
2020-09-26 23:32:29 +0530

    reduce and map

--- revision.js
+++ revision.js
@@ -17,15 +17,32 @@
 // spread operator
 // expanding arrays
 
-let greet = ['hello', 'world'];
-console.log(...greet);
-let word = 'hellow world'
-console.log([...word]);
+// let greet = ['hello', 'world'];
+// console.log(...greet);
+// let word = 'hellow world'
+// console.log([...word]);
 
-const sum = (num1, num2, num3) => {
-    let result = num1 + num2 + num3;
-    return result;
+// const sum = (num1, num2, num3) => {
+//     let result = num1 + num2 + num3;
+//     return result;
+// }
+
+// let arr = [1, 2, 3];
+// console.log(sum(...arr));
+
+// array.map function
+// pass function to map
+
+const arr = [1, 4, 9, 16];
+// const map1 = arr.map(x => x * 2);
+// console.log(map1);
+//Array.reduce
+
+let red = arr.reduce(fun)
+console.log(red);
+
+function fun(acc, val) {
+    console.log('acc', acc);
+    console.log('val', val);
+    return acc + val;
 }
-
-let arr = [1, 2, 3];
-console.log(sum(...arr));
