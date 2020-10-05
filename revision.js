82ee68a28b0231602828fc7e35338302144a0834
akhil-kk <akhilkkopparambil@gmail.com>
2020-10-05 18:19:53 +0530

    js revision

--- revision.js
+++ revision.js
@@ -58,22 +58,108 @@
 // console.log(volume(3));
 
 // iterator
-function  nameIterator(names) {
-    let nextIndex = 0;
+// function  nameIterator(names) {
+//     let nextIndex = 0;
 
-    return {
-        next: function() {
-            return nextIndex < names.length ?
-            {value: names[nextIndex++], done: false} :
-            {done: true}
-        }
-    }
+//     return {
+//         next: function() {
+//             return nextIndex < names.length ?
+//             {value: names[nextIndex++], done: false} :
+//             {done: true}
+//         }
+//     }
+// }
+
+
+// const namesArr = ['jack', 'jill', 'john'];
+// const names = nameIterator(namesArr);
+
+// console.log(names.next().value);
+// console.log(names.next().value);
+// console.log(names.next().value);
+
+// let obj = {
+//     name: "Akhil",
+//     number: 12
+// }
+
+// const {name, number} = obj;
+
+// console.log(name, number);
+
+
+// iterators
+
+// function nameIterator(names) {
+//     let nextIndex = 0;
+//     return {
+//         next: function() {
+//             return nextIndex < names.length ? 
+//             {value: names[nextIndex++], done: false} : 
+//             {done: true};
+//         }
+//     }
+// }
+
+let namesArr = ['kk', 'arjun', 'nikhil'];
+let names = nameIterator(namesArr);
+console.log(names.next());
+console.log(names.next());
+console.log(names.next());
+console.log(names.next());
+
+const makeCounter = () => {
+    let count = 0;
+    const counter = () => count++;
+    return counter;
 }
 
+let cnt = makeCounter();
+console.log(cnt());
+console.log(cnt());
+console.log(cnt());
 
-const namesArr = ['jack', 'jill', 'john'];
-const names = nameIterator(namesArr);
 
-console.log(names.next().value);
-console.log(names.next().value);
-console.log(names.next().value);
+let arr = 'abc, @kjdwldhlw @ adkjdhww @ldh';
+let x = arr.split(',');
+console.log(x.join(','));
+
+function fun(acc, val) {
+    console.log('acc', acc);
+    console.log('val', val);
+    return acc + val;
+}
+const arr = [1, 4, 9, 16];
+let red = arr.reduce(fun)
+console.log(red);
+
+let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        reject('hello world')
+    }, 1000)
+});
+
+promise.then((data => console.log(data)));
+
+const promise = new Promise((resolve, reject) => {
+    if(true) {
+        resolve('success');
+    } else {
+        reject('failed');
+    }
+})
+
+promise
+.then(data => {
+    throw Error}
+    ).catch(()=> console.log('error'));
+
+
+
+let arr = [12, 34, 54, 2, 5];
+let x = arr.sort((a, b) => (b - a));
+console.log(x);
+
+setInterval(() => {
+    console.log('hi')
+}, 1000);
